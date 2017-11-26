2de02fdea88ad263b50bbde2dc07ca37218c4fc6
LayersSss <layerssss@gmail.com>
2017-11-26 16:23:43 +1300

    Setup favicon

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,11 @@ export default class MyDocument extends Document {
     return (
       <html lang="zh">
         <Head>
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            href="/static/favicon.ico"
+          />
           <meta
             name="viewport"
             content={
