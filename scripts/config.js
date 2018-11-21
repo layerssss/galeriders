682bd0f67814d83e391609048280058374eb6b17
Michael Yin <layerssss@gmail.com>
2018-11-21 16:49:26 +1300

    Remove ssr

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
-const apiOrigin = dev ? 'http://grapi.test' : 'http://api.galeriders.club';
+const apiOrigin = dev ? 'https://grapi.test' : 'https://api.galeriders.club';
 const origin = dev ? 'https://galeriders.test' : 'https://galeriders.club';
 
 const config = {
--- scripts/server.js
+++ scripts/server.js
@@ -30,7 +30,7 @@ runAsync(async () => {
     '/graphql',
     httpProxyMiddleware({
       target: `${config.apiOrigin}`,
-      changeOrigin: true,
+      xfwd: true,
       agent: apiAgent,
     })
   );
