4c135c59415ee0bb438e42eb4056c6ea3abf0248
Michael Yin <me@micy.in>
2021-08-23 13:53:00 +1200

    amend dev_restore_backup

--- gr_api/bin/dev_restore_backup.sh
+++ gr_api/bin/dev_restore_backup.sh
@@ -1,1 +1,12 @@
-cat ~/ownCloud/backup/grapi/grapi.pgdump | pg_restore --clean -d grapi_development
+#!/usr/bin/env bash
+set -ex
+
+FMS_DUMP_URL=https://dav.micy.in/backup_dbdumps/grapi/grapi.pgdump
+
+if [ ! -f tmp/prod.pgdump ]
+then
+    wget $FMS_DUMP_URL -O tmp/prod.pgdump
+fi
+
+cat tmp/prod.pgdump | pg_restore --clean --no-owner -d grapi_development
+
